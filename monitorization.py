59c0f05ac520d93189a46cab59ed9a0b7eb0d8f6
IgnasiB <ignasib@IgnasiB-2.local>
2017-05-30 13:04:37 +0200

    First commit

---
+++ monitorization.py
@@ -0,0 +1,131 @@
+'''
+    This script is for monitoring both jenkins and devel tomcat 
+    of the back-end department of Vicens Vives Digital.
+    
+    The aim is to send the status of all jenkins jobs and the tomcat running on devel environment.
+    
+    The jenkins jobs to be monitored are all declared in "jobs" array:
+        jobs = ["engine_api_ci_devel", "user_api_ci_devel", "engine_db_clean_devel", "engine_php_sdk_ci", "peacemaker_ci", "bff_ci_devel", "bfa_ci_devel"]
+    
+    Tomcat is running on http://devel:8080
+    
+    INPUT: no input
+    OUTPUT: {jenkins_job_status}-{tomcat_status}
+    WHERE: jenkins_status : {'b', 'r', 'a', 'y'}
+           tomcat_status : {'1', '0'}
+    
+    When one of the jobs' result is different than "SUCCESS", it takes control of the NodeMcu LEDs
+    until its status is SUCCESS again.
+
+    We send packets using UDP transport layer protocol.
+
+    Author: Ignasi Ballara - VVD
+    Version: 4.0 - Including UDP.
+
+'''
+
+import json
+import sys
+import urllib2
+import time
+import socket
+import requests
+
+# Configurations
+ping_server = 1
+ping_when_not_success = 30
+NAME = 0
+BUILD = 1
+UDP_IP = "192.168.2.23" # Address of NodeMcu
+UDP_PORT = 2390 # Port of NodeMcu (arbitrary)
+sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) 
+# Jobs to be monitored.
+jobs = ["engine_api_ci_devel", "user_api_ci_devel", "engine_db_clean_devel", "engine_php_sdk_ci", "peacemaker_ci", "bff_ci_devel", "bfa_ci_devel"]
+
+# Arduino Configuration
+SUCCESS = 'b'
+FAILURE = 'r'
+BUILDING = 'a'
+UNSTABLE = 'y'
+TOMCAT_OK = '1'
+TOMCAT_NO_OK = '0'
+time.sleep(5)
+
+# Returns the status of a job ans its name.
+def get_status_of_job(jobName):
+    jenkinsUrl = "http://jenkins.vicensvivesdigital.com:10080/job/"
+    try:
+        request = urllib2.Request( jenkinsUrl + jobName + "/lastBuild/api/json", headers={"Authorization" : "Basic am9hcXVpbS5jYWx2bzpqb2FxdWltVlYyMDE2"} )
+        contents = urllib2.urlopen(request)
+    except urllib2.HTTPError, e:
+        print "URL Error: " + str(e.code)
+        print "      (job name [" + jobName + "] probably wrong)"
+        sys.exit(2)
+    try:
+        buildStatusJson = json.load(contents)
+    except:
+        print "Failed to parse json"
+        sys.exit(3)
+
+    build_status = buildStatusJson["result"]
+
+    if build_status == "SUCCESS":
+        status = SUCCESS
+    elif build_status == None:
+        status = BUILDING
+    elif build_status == "FAILURE":
+        status = FAILURE
+    elif build_status == "UNSTABLE":
+        status = UNSTABLE
+
+    print ""
+    print "----------NEW--DATA-----------"
+    if build_status != None:
+        print "Job " + jobName + ": " + build_status
+    else:
+        print "Job " + jobName + " is building"
+
+    return jobName, status
+
+#Returns the status of the devel tomcat.
+def check_devel_tomcat():
+    try:
+        status = requests.get('http://devel:8080').status_code
+    except requests.exceptions.HTTPError:
+        status = 500
+    except requests.exceptions.ConnectionError:
+        status = 500
+
+    if status == 200:
+        return TOMCAT_OK
+    else:
+        return TOMCAT_NO_OK
+
+# MAIN
+while(1):
+
+    for job in jobs:
+        job_status = get_status_of_job(job)
+
+        # If a job status is != from SUCCESS, it will take control of the UDP comm.
+        while job_status[BUILD] != SUCCESS:
+            # We check the Tomcat's status.
+            tomcat_status = check_devel_tomcat()
+            print "Tomcat Status: " + tomcat_status
+            # We set the data we will send to arduino.
+            data_to_send = job_status[BUILD] + "-" + tomcat_status
+            print "data_to_send: " + data_to_send
+            # We send the data. Typical: 'b-1'
+            sock.sendto(data_to_send, (UDP_IP, UDP_PORT))
+            # We wait for some seconds before relaunching the request.
+            time.sleep(ping_when_not_success)
+            # We check the status of the job.
+            job_status = get_status_of_job(job)
+
+        tomcat_status = check_devel_tomcat()
+        print "Tomcat Status: " + tomcat_status
+        data_to_send = job_status[BUILD] + "-" + tomcat_status
+        print "data_to_send: " + data_to_send
+        sock.sendto(data_to_send, (UDP_IP, UDP_PORT))
+        time.sleep(ping_server)
+    
---
+++ monitorization_nodeMcu_udp/monitorization_nodeMcu_udp.ino
@@ -0,0 +1,221 @@
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#include <Servo.h>
+
+/*
+   Programa per rebre de l'Script de Jenkins l'estat de la Build.
+*/
+int incomingByte = 0;
+int jenkins_count = 1;
+int tomcat_count = 1;
+
+//WIFI 
+const char* ssid = "IgnasiB";
+
+// UDP variables
+WiFiUDP Udp;
+char packetBuffer[255]; //buffer to hold incoming packet
+const int localPort = 2390;
+int packetSize;
+char inBytes[4];
+
+// GPIO PORTS
+const int led_red = 2;
+const int led_yellow1 = 12;
+const int led_yellow2 = 14;
+const int led_green1 = 15;
+const int led_green2 = 13;
+const int tomcat_led_green = 0;
+const int tomcat_led_red = 4;
+const int servo_pin = 5;
+Servo servo;
+
+void setup() {
+  pinMode(led_red, OUTPUT);
+  pinMode(led_green1, OUTPUT);
+  pinMode(led_green2, OUTPUT);
+  pinMode(led_yellow1, OUTPUT);
+  pinMode(led_yellow2, OUTPUT);
+  pinMode(tomcat_led_green, OUTPUT);
+  pinMode(tomcat_led_red, OUTPUT);
+  servo.attach(servo_pin);
+  Serial.begin(9600); // Opens serial port, sets data rate to 9600 bps
+  connect_to_wifi();
+  connect_UDP();
+  welcome();
+}
+
+void loop() {
+
+  packetSize = Udp.parsePacket();
+  if (packetSize) { 
+    packetBuffer[0] = '\0';
+    packetBuffer[1] = '\0';
+    packetBuffer[2] = '\0';
+    packetBuffer[3] = '\0';
+    int len = Udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
+    if (len > 0) {
+        packetBuffer[len] = '\0';
+    }
+  }
+  
+  if (packetBuffer[0] == 'b') {
+    jenkins_green();
+    jenkins_count = 1;
+  }
+  else if (packetBuffer[0] == 'y') {
+    jenkins_yellow();
+    jenkins_count = 1;
+  }
+  else if (packetBuffer[0] == 'r') {
+    jenkins_red();
+    if (jenkins_count == 1) {
+      ring_bell();
+      jenkins_count = 0;
+    }
+  }
+  else if (packetBuffer[0] == 'a') {
+    jenkins_building();
+    jenkins_count = 1;
+  }
+
+  if (packetBuffer[2] == '1') {
+    tomcat_green();
+    tomcat_count = 1;
+  }
+  else if (packetBuffer[2] == '0') {
+    tomcat_red();
+    if (tomcat_count == 1) {
+      ring_bell();
+      tomcat_count = 0;
+    }
+  }
+}
+
+void jenkins_green() {
+  digitalWrite(led_green1, HIGH);
+  digitalWrite(led_green2, HIGH);
+  digitalWrite(led_yellow1, LOW);
+  digitalWrite(led_yellow2, LOW);
+  digitalWrite(led_red, LOW);
+  delay(1000);
+}
+
+void jenkins_yellow() {
+  digitalWrite(led_green1, LOW);
+  digitalWrite(led_green2, LOW);
+  digitalWrite(led_yellow1, HIGH);
+  digitalWrite(led_yellow2, HIGH);
+  digitalWrite(led_red, LOW);
+  delay(1000);
+}
+
+void jenkins_red() {
+  digitalWrite(led_green1, LOW);
+  digitalWrite(led_green2, LOW);
+  digitalWrite(led_yellow1, LOW);
+  digitalWrite(led_yellow2, LOW);
+  digitalWrite(led_red, HIGH);
+  delay(1000);
+}
+
+void jenkins_building() {
+  // ANADA
+  digitalWrite(led_green1, HIGH);
+  delay(100);
+  digitalWrite(led_green2, HIGH);
+  delay(100);
+  digitalWrite(led_yellow1, HIGH);
+  delay(100);
+  digitalWrite(led_yellow2, HIGH);
+  delay(100);
+  digitalWrite(led_red, HIGH);
+  delay(175);
+  // TORNADA
+  digitalWrite(led_green1, LOW);
+  delay(100);
+  digitalWrite(led_green2, LOW);
+  delay(100);
+  digitalWrite(led_yellow1, LOW);
+  delay(100);
+  digitalWrite(led_yellow2, LOW);
+  delay(100);
+  digitalWrite(led_red, LOW);
+  delay(100);
+}
+
+void ring_bell() {
+  //servo.write(0);
+  //delay(300);
+  //servo.write(180);
+}
+
+void tomcat_green() {
+  digitalWrite(tomcat_led_green, HIGH);
+  digitalWrite(tomcat_led_red, LOW);
+}
+
+void tomcat_red() {
+  digitalWrite(tomcat_led_green, LOW);
+  digitalWrite(tomcat_led_red, HIGH);
+}
+
+
+/**********************************
+ ******** SETUP METHODS ***********
+ *********************************/
+
+void connect_to_wifi() {
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present"); 
+    while(true);  // don't continue
+  } 
+
+  // attempt to connect to Wifi network:
+  Serial.print("Attempting to connect to SSID: ");
+  Serial.println(ssid);
+  WiFi.begin(ssid);
+  while ( WiFi.status() != WL_CONNECTED) { 
+    delay(500);
+    Serial.print(".");
+  }
+  // print your WiFi shield's IP address:
+  Serial.println("CONNECTED!");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void connect_UDP(){
+  Serial.print("");
+  Serial.println("Connecting to UDP");
+
+  if(Udp.begin(localPort) == 1) {
+    Serial.println("Connetion succesful");
+  } else {
+    Serial.println("Connection failed");
+  }
+}
+
+void welcome() {
+
+  for (int i = 0; i < 5; i++) {
+    digitalWrite(led_red, HIGH);
+    digitalWrite(led_green1, HIGH);
+    digitalWrite(led_green2, HIGH);
+    digitalWrite(led_yellow1, HIGH);
+    digitalWrite(led_yellow2, HIGH);
+    digitalWrite(tomcat_led_green, HIGH);
+    digitalWrite(tomcat_led_red, HIGH);
+    delay(200);
+    digitalWrite(led_red, LOW);
+    digitalWrite(led_green1, LOW);
+    digitalWrite(led_green2, LOW);
+    digitalWrite(led_yellow1, LOW);
+    digitalWrite(led_yellow2, LOW);
+    digitalWrite(tomcat_led_green, LOW);
+    digitalWrite(tomcat_led_red, LOW);
+    delay(200);
+  }
+  //servo.write(180);
+}
+
